d21c40fa416abdfa7a599ad697211649e88f2a92
jacobRozak <jakub.rozi.rozak@gmail.com>
2023-01-26 16:17:25 +0000

    cleanup

--- src/@types/assets.d.ts
+++ src/@types/assets.d.ts
@@ -1,13 +1,3 @@
-declare module "*.svg" {
-    const content: any;
-    export default content;
-  }
-  
-  declare module "*.png" {
-    const content: any;
-    export default content;
-  }
-  
   type ContextType = {
     dark: boolean;
     changeTheme: () => void;
